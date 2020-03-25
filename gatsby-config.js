c3ac6d943fa7c66b24a69ac0d919f8bdd75f6173
adedaniel <adetoladaniel693@gmail.com>
2020-03-25 13:06:12 +0100

    First commit

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,6 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
+    title: `adedaniel`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
     author: `@gatsbyjs`,
   },
@@ -18,8 +18,8 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `adedaniel`,
+        short_name: `adedaniel`,
         start_url: `/`,
         background_color: `#663399`,
         theme_color: `#663399`,
